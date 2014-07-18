004d294b1d141972ca3786db5bef482c95625eda
mik23 <marcotrigiano.m@gmail.com>
2014-07-18 15:01:14 +0200

    ProgramingAssignment2
    R programming

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inversa) inv <<- inversa
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +22,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  if(!is.null(x$get()) && is.matrix(x$get())){
+    squared <- dim(x$get())[1] == dim(x$get())[2]
+    if(squared == T){
+      matrix <- x$get()
+      inv <- solve(matrix)
+      x$setinv(inv)
+    }
+    else
+      message("matrix must be sqare")
+    
+  }
+  else
+    message("This is not a matrix!")
+  inv
 }
